36b1f56e858aa1913fad2c5e6ea5ab4882bf5b2f
alnahian <nahian33@yahoo.com>
2013-11-15 22:47:50 +0600

    after some view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  default_scope :order => 'title'
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
   
   def self.search(search)
     if search
@@ -8,5 +12,16 @@ class Product < ActiveRecord::Base
       scoped
     end
   end
+  
+  private
+	# ensure that there are no line items referencing this product
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end
+	end
   
 end
